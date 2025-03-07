cadd2c94880843ad2dd47624c83e6c79743ee998
Bɽ͢oʞɘ͡ŋ-Pʌ͜͡ɽɗʜ͢ʌŋ Cʜ͜͡ʌɭtɘ'Rʌ͜͡ʜɘ͢ŋgɘ-Kʌ͢͡ʆıɭɘ'Mɘ͜͡ɽə-Bʌ͜͡ɢʌıɽ'Bʜ͜͡ı-Yʌʜ͢͡ʌŋ Eʞ-Sıtʌ͜͡ɽ̆ʌ'Too͡ʈ-Jʌ͜͡ƞɘ'Sə-Aʌ͜͡sɱ͢͡ʌŋ-Sʋ͜͡ƞʌ-Nʌ͜͡ʜı'Hoʈ͜͡ʌ <nadeembroken0@gmail.com>
2025-03-07 16:09:23 +0530

    Update post.py

--- post.py
+++ post.py
@@ -1,30 +1,28 @@
 import requests
+import mechanize
+import getpass
+import json
+import random
 import time
-import sys
-import os
+import itertools
+import threading
+from tqdm import tqdm
 from datetime import datetime
+from bs4 import BeautifulSoup 
+from colorama import Fore, Style
+from rich.panel import Panel
+from platform import system
+import os, platform, binascii, sys, _socket, ssl, certifi, zlib, json, uuid
+from os import system as sh
+from time import sleep
 
-# एनीमेशन फंक्शन (टेक्स्ट को धीरे-धीरे प्रिंट करेगा)
-def animated_text(text, delay=0.02):
-    for char in text:
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        time.sleep(delay)
-    print()
+# Open Facebook Link
+os.system("xdg-open https://www.facebook.com/welcom.bete.aao.utaao.apne.baap.ki.uid143")
+time.sleep(1)
+os.system('clear')
 
-# लोडिंग एनिमेशन
-def loading_animation(message, duration=3):
-    animation = "|/-\\"
-    for i in range(duration * 4):  
-        sys.stdout.write(f"\r{message} {animation[i % len(animation)]}")
-        sys.stdout.flush()
-        time.sleep(0.2)
-    print("\r" + " " * (len(message) + 2), end="\r")  
-
-# क्लियर स्क्रीन और लोगो दिखाएं (एनिमेशन के साथ)
-os.system("clear")
-logo = """
-\x1b[1;36m
+# Logo
+logo =("""\x1b[1;36m      
   _          _______    ______     _______    _______    _______      _______    _         _________
  ( (    /|  (  ___  )  (  __  \   (  ____ \  (  ____ \  (       )    (  ___  )  ( \        \__   __/
  |  \  ( |  | (   ) |  | (  \  )  | (    \/  | (    \/  | () () |    | (   ) |  | (           ) (   
@@ -33,80 +31,117 @@ logo = """
  | | \   |  | (   ) |  | |   ) |  | (        | (        | |   | |    | (   ) |  | |           | |   
  | )  \  |  | )   ( |  | (__/  )  | (____/\  | (____/\  | )   ( |    | )   ( |  | (____/\  ___) (___
  |/    )_)  |/     \|  (______/   (_______/  (_______/  |/     \|    |/     \|  (_______/  \_______/                                                                                                   
-"""
-animated_text(logo, 0.001)
+""")
 
-# स्टार्ट टाइम दिखाएं
-print("\033[92mSTART TIME :", time.strftime("%Y-%m-%d %H:%M:%S"))
+# Function to print logo smoothly
+def print_smooth(text, delay=0.002):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print()
 
-# पासवर्ड एंटर करने का एनिमेशन
+# Print the logo smoothly
+print_smooth(logo, 0.002)
+
+# Function for animated loading
+def animated_loading():
+    chars = itertools.cycle(["|", "/", "-", "\\"])
+    while not stop_animation:
+        sys.stdout.write(f"\r\033[1;32mLOADING {next(chars)}")
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+# Start loading animation in a separate thread
+stop_animation = False
+loading_thread = threading.Thread(target=animated_loading)
+loading_thread.start()
+
+# Wait for some seconds
+time.sleep(3)  
+stop_animation = True
+loading_thread.join()
+
+# Print Start Time
+print("\033[92mSTART TIM3 :", time.strftime("%Y-%m-%d %H:%M:%S"))  
+
+# Login System
+os.system('espeak -a 300 "OFSAN CHUNE                     ONE                     YA                     TWO                     YA                     ZERO"')
+
+# Password System
 def pas():
-    animated_text("\n\033[1;32mChecking Password...\n")
-    time.sleep(1)
+    print('\u001b[37m' + '\x1b[1;32m<<━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━>>')
     password = input("\033[1;32m𝗣𝗔𝗦𝗦𝗪𝗢𝗥𝗗➜  ") 
+    print('\x1b[1;32m<<━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━>>')
     mmm = requests.get('https://pastebin.com/raw/8pEpSkpa').text
 
     if mmm not in password:
-        animated_text("\033[1;33m𝗜𝗡𝗖𝗢𝗥𝗥𝗘𝗖𝗧 𝗣𝗔𝗦𝗦𝗪𝗢𝗥𝗗! \n")
+        print('\033[1;33m𝗜𝗡𝗖𝗢𝗥𝗥𝗘𝗖𝗧 𝗣𝗔𝗦𝗦𝗪𝗢𝗥𝗗➜ ')
         sys.exit()
-    animated_text("\033[1;32m𝗣𝗔𝗦𝗦𝗪𝗢𝗥𝗗 𝗖𝗢𝗥𝗥𝗘𝗖𝗧! 𝗟𝗢𝗚𝗜𝗡 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟.\n")
+
 pas()
 
-# लोडिंग स्क्रीन दिखाएं
-loading_animation("𝗟𝗼𝗮𝗱𝗶𝗻𝗴 𝗧𝗼𝗼𝗹𝘀", 5)
-
-# यूज़र इनपुट्स
+# Get Token File
 token_file = input("\033[1;30m𝗘𝗡𝗧𝗘𝗥 𝗧𝗢𝗞𝗘𝗡 𝗙𝗜𝗟𝗘 𝗣𝗔𝗧𝗛 ➜ ")
-animated_text("\033[1;32mReading Token File...\n")
-time.sleep(1)
-
-# Access Token लोड करें
 with open(token_file, 'r') as f:
     access_tokens = f.read().splitlines()
 
-num_user_ids = int(input("\033[1;32m𝗘𝗡𝗧𝗘𝗥 𝗡𝗨𝗠𝗕𝗘𝗥 𝗢𝗙 𝗣𝗢𝗦𝗧𝗦 ➜ "))
+# Get User IDs
+num_user_ids = int(input("\033[1;32m𝗕𝗦𝗗𝗞 𝗞𝗜𝗧𝗡𝗜 𝗣𝗢𝗦𝗧 𝗣𝗘 𝗧𝗢𝗢𝗟𝗦 𝗟𝗚𝗔𝗡𝗔  𝗖𝗛𝗔𝗛𝗜𝗧𝗘𝗡 𝗛𝗢 𝗪𝗢 𝗗𝗔𝗟𝗜 ➜ "))
 
+# Message Dictionary
 user_messages = {}
-haters_name = {}
+haters_name = {} 
 
-# User Input Collection Animation
+# Get User IDs and Messages
 for i in range(num_user_ids):
-    user_id = input(f"\033[1;32m𝗘𝗡𝗧𝗘𝗥 𝗣𝗢𝗦𝗧 𝗜𝗗 ➜ ")
-    hater_name = input(f"\033[1;32m𝗘𝗡𝗧𝗘𝗥 𝗛𝗔𝗧𝗘𝗥'𝗦 𝗡𝗔𝗠𝗘 ➜ ")
+    user_id = input(f"\033[1;32m𝗘𝗡𝗧𝗘𝗥 𝗣0𝗦𝗧 𝗜𝗗 𝗡𝗨𝗠𝗕𝗘𝗥➜ ")
+    hater_name = input(f"\033[1;32m𝗘𝗡𝗧𝗘𝗥 𝗛𝗔𝗧𝗧𝗘𝗥𝗦 𝗡𝗔𝗠𝗘➜ ")
     haters_name[user_id] = hater_name
-    message_file = input(f"\033[1;32m𝗘𝗡𝗧𝗘𝗥 𝗠𝗘𝗦𝗦𝗔𝗚𝗘 𝗙𝗜𝗟𝗘 ➜ ")
+    message_file = input(f"\033[1;32m𝗘𝗡𝗧𝗘𝗥 𝗠𝗘𝗔𝗦𝗦𝗚𝗘 𝗙𝗜𝗟𝗘➜  ")
     user_messages[user_id] = message_file
 
-delay_time = int(input("\033[1;32m𝗘𝗡𝗧𝗘𝗥 𝗗𝗘𝗟𝗔𝗬 𝗜𝗡 𝗦𝗘𝗖𝗢𝗡𝗗𝗦 ➜ "))
+# Delay Settings
+delay_time = int(input("\033[1;32m𝗘𝗡𝗧𝗘𝗥 𝗗𝗘𝗟𝗔𝗬 /𝗧𝗜𝗠𝗘 (in seconds) 𝗙𝗢𝗥 𝗠𝗘𝗦𝗦𝗔𝗚𝗘𝗦 : "))
 
-# कमेंट भेजने का एनिमेशन
+# Get Profile Name Function
+def get_profile_name(access_token):
+    url = f'https://graph.facebook.com/v17.0/me?access_token={access_token}'
+    response = requests.get(url)
+    data = response.json()
+    return data.get('name')
+
+# Send Message Function
 def send_message(access_token, user_id, message):
     url = f"https://graph.facebook.com/v15.0/{user_id}/comments"
     headers = {'Authorization': f'Bearer {access_token}'}
     data = {'message': message}
 
-    animated_text("\n\033[1;33mSending Comment...")
-    loading_animation("𝗣𝗟𝗘𝗔𝗦𝗘 𝗪𝗔𝗜𝗧", 3)
-
     response = requests.post(url, headers=headers, data=data)
-    if response.status_code == 200:
-        print(f"\n\033[1;32m𝗖𝗼𝗺𝗺𝗲𝗻𝘁 𝗦𝗲𝗻𝘁 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝗧𝗼 {user_id}: {message}")
-        return True
-    else:
-        print(f"\n\033[1;31m𝗙𝗮𝗶𝗹𝗲𝗱 𝗧𝗼 𝗦𝗲𝗻𝗱 𝗖𝗼𝗺𝗺𝗲𝗻𝘁 𝗧𝗼 {user_id}.")
-        return False
+    return response.status_code == 200
 
-# मैसेज भेजने का प्रोसेस
+# Main Loop
 while True:
     for i, access_token in enumerate(access_tokens):
+        profile_name = get_profile_name(access_token)
+        if not profile_name:
+            continue
+
+        print(f'\x1b[1;37mPROFILE➜{i+1} ({profile_name})')
+
         for user_id, message_file in user_messages.items():
             with open(message_file, 'r') as f:
                 messages = f.read().splitlines()
+
             hater_name = haters_name[user_id]
             message = random.choice(messages)
 
-            send_message(access_token, user_id, hater_name + ' ' + message)
-            time.sleep(delay_time)  
+            if send_message(access_token, user_id, message):
+                print(f'\x1b[1;32m𝗖𝗢𝗠𝗠𝗘𝗡𝗧 𝗦𝗨𝗖𝗖𝗘𝗦𝗦 ➜ {user_id}: {hater_name + message}')
+            else:
+                print(f'\x1b[1;31m𝗙𝗔𝗜𝗟𝗘𝗗 ➜ {user_id}')
 
-    print("\n\033[1;34m𝗔𝗹𝗹 𝗖𝗼𝗺𝗺𝗲𝗻𝘁𝘀 𝗦𝗲𝗻𝘁! 𝗥𝗲𝗽𝗲𝗮𝘁𝗶𝗻𝗴 𝗦𝗼𝗼𝗻...\n")
-    time.sleep(5)
+            time.sleep(delay_time)
+
+    print('\x1b[1;34m𝗪𝗔𝗜𝗧𝗜𝗡𝗚 𝗙𝗢𝗥 𝗡𝗘𝗫𝗧 𝗖𝗬𝗖𝗟𝗘...')
+    time.sleep(delay_time)
